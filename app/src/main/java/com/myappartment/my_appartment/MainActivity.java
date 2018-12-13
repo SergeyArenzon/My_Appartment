56a28b4f81d8bc3a155acc9422b6a9a7d29e61c9
SergeyArenzon <sergeyr1991@gmail.com>
2018-12-13 16:07:09 +0200

    Changed database base to Firebase

--- app/src/main/java/com/myappartment/my_appartment/MainActivity.java
+++ app/src/main/java/com/myappartment/my_appartment/MainActivity.java
@@ -5,20 +5,45 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.myappartment.my_appartment.maincode.Pages;
+import com.myappartment.my_appartment.maincode.Sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-    static boolean permission;
+
+    //Intent goToRequests;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // goToRequests = new Intent(MainActivity.this,requestsList.class);
          final AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
+
 
             ///LOGIN BUTTON///
 
@@ -26,22 +51,20 @@ public class MainActivity extends AppCompatActivity {
         Button lgnBtn = (Button)findViewById(R.id.loginBtn);
         lgnBtn.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View v) {
-        v.startAnimation(buttonClick);
-
-                ///(IF USER IS RENTER)///
-                Intent goToRequests = new Intent(MainActivity.this,requestsList.class);
-                startActivity(goToRequests);
-
-                ///(IF USER IS RENTING)///
+                v.startAnimation(buttonClick);
+                User user = new User("sergey","124","aren",true);
 
 
+                DatabaseReference myRef = database.getReference();
+                myRef.setValue(user);
 
 
+         }
 
 
 
-            }
         });
 
 
@@ -51,6 +74,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.startAnimation(buttonClick);
+
+
+
+
+
+
+
+
 
                 //AFTER "REGISTER" BTN CLICK//
                 //CHOOSE RENTING/RENTER///
@@ -64,9 +95,16 @@ public class MainActivity extends AppCompatActivity {
                         // the user clicked on "who are you?"
 
                         if (which == 0) {
+
+
+
+
                             Intent renterReg = new Intent(com.myappartment.my_appartment.MainActivity.this, renterReg.class);
                             startActivity(renterReg);
                         } else {
+
+
+
                            Intent rentingReg = new Intent(com.myappartment.my_appartment.MainActivity.this, rentingReg.class);
                            startActivity(rentingReg);
 
--- app/src/main/java/com/myappartment/my_appartment/renterReg.java
+++ app/src/main/java/com/myappartment/my_appartment/renterReg.java
@@ -6,18 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class renterReg extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    EditText email,pass,name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         final AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_renter_reg);
-
         Button cnclBtn = (Button) findViewById(R.id.renterRegCancle);
         Button cfmBtn = (Button) findViewById(R.id.renterRegConfirm);
-
+        email =(EditText)findViewById(R.id.renterRegUser);
+        pass =(EditText)findViewById(R.id.renterRegPass);
+        name =(EditText)findViewById(R.id.renterRegName);
 
                     ///CANCLE BUTTON///
         cnclBtn.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +50,9 @@ public class renterReg extends AppCompatActivity {
             public void onClick(View v) {
                 v.startAnimation(buttonClick);
                 ///(if user registration veryfied)///
+                User user = new User(email.getText().toString(),pass.getText().toString(),name.getText().toString(),false);
+                FirebaseUser User = mAuth.getCurrentUser();
+                mAuth.createUserWithEmailAndPassword(user.getEmail(), user.getPass());
 
                 Intent goRequests = new Intent(renterReg.this,requestsList.class);
                 startActivity(goRequests);
